0a3d13c7f46dabd01638f536cb7927361224a182
Leandro <102317613+Leme-do-Prado@users.noreply.github.com>
2025-02-11 14:05:12 -0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,8 @@
+
+print("Hello! Let's generate your band name. \n"
+      + "Im going to ask you some questions now.")
+cityName=input("What's the city you grew up in? \n")
+petName=input("What's your favourite animal? \n")
+
+print("Your band name is " + cityName + " " + petName + "s!")
+
